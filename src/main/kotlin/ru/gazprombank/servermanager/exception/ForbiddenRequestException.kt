dd594ebaeaf42f2f5a55628a9a1d71042016ca9c
HackMemory <dragon.warri0r@yandex.com>
2024-02-21 15:10:26 +0000

    feat: add exceptions

---
+++ src/main/kotlin/ru/gazprombank/servermanager/exception/ForbiddenRequestException.kt
@@ -0,0 +1,7 @@
+package ru.gazprombank.servermanager.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+class ForbiddenRequestException : RuntimeException()
